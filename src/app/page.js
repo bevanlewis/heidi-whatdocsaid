714f1de66600f0c58cfa2dd854267ddaf49c4d5c
Nathan <s3993380@student.rmit.edu.au>
2025-07-12 14:00:37 +1000

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
+"use client";
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
+import Summary from "../components/summary";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <Header/>
-      <Sidebar/>
-      <main className="flex-grow">
-        {/* Main content goes here */}
-      </main>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <div className="flex-1 px-12 py-10 bg-[#F3F3F3]">
+          <Summary />
+        </div>
+      </div>
     </div>
   );
 }
